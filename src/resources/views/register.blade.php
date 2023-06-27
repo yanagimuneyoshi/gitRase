b8ee38660566dde2f458b6860fa52bbbccccfc5c
yanagimuneyoshi <yanagimuneyoshi1994gmail.com>
2023-06-27 22:41:18 +0900

    2022/6/27＿22:40

---
+++ src/resources/views/register.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <header>
+    <div class="menu-button">
+      <button id="btn_menu8" class="btn_menu" href="#"><span>MENU</span></button>
+    </div>
+    <div class="rese">
+      <a>RESE</a>
+    </div>
+  </header>
+
+  <div class="login-form">
+    <h2>Login</h2>
+    <input type="name" placeholder="username">
+    <input type="text" placeholder="Email">
+    <input type="password" placeholder="Password">
+    <button type="submit" class="login-button">Login</button>
+  </div>
+</body>
+
+</html>
