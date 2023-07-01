87714de05fc3e10d426962f6c0e2fd7de0280bf6
yanagimuneyoshi <yanagimuneyoshi1994gmail.com>
2023-07-01 23:28:52 +0900

    2023/7/1_23:28

---
+++ src/resources/views/shop_all.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+
+
+  <body>
+    <h1>お店一覧</h1>
+    <div class="container">
+      <div class="shop">
+        <h3>店舗名1</h3>
+        <p>#1</p>
+        <a href="#">詳しく見る</a>
+      </div>
+      <div class="shop">
+        <h3>店舗名2</h3>
+        <p>#2</p>
+        <a href="#">詳しく見る</a>
+      </div>
+      <div class="shop">
+        <h3>店舗名3</h3>
+        <p>#3</p>
+        <a href="#">詳しく見る</a>
+      </div>
+      <div class="shop">
+        <h3>店舗名4</h3>
+        <p>#4</p>
+        <a href="#">詳しく見る</a>
+      </div>
+      <!-- 他のお店の情報を同様に追加 -->
+    </div>
+  </body>
+
+</html>
+
+</body>
+
+</html>
